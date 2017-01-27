39929825ac0ac7e76357225781db7d9799d95f9d
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-01-28 00:16:45 +0800

    Do not allow AndroidUtils to be initialized because it is a utility class.

--- src/main/java/com/emmanuelcorrales/android/utils/AndroidUtils.java
+++ src/main/java/com/emmanuelcorrales/android/utils/AndroidUtils.java
@@ -9,6 +9,10 @@ import static android.content.Context.INPUT_METHOD_SERVICE;
 
 public class AndroidUtils {
 
+    private AndroidUtils() {
+        //Android Utils should never be initialized.
+    }
+
     public static void hideKeyboard(Context context, View view) {
         InputMethodManager imm = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
