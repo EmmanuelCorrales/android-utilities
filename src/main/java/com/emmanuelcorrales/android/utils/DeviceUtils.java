c5d4978c69748e8ffd4286fb26aaee85bb082f25
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-06-01 02:56:30 +0800

    Add vibrate method.

--- src/main/java/com/emmanuelcorrales/android/utils/DeviceUtils.java
+++ src/main/java/com/emmanuelcorrales/android/utils/DeviceUtils.java
@@ -1,9 +1,26 @@
 package com.emmanuelcorrales.android.utils;
 
 
+import android.content.Context;
+import android.os.Vibrator;
+
 public class DeviceUtils {
 
-    private DeviceUtils(){
+    private static long DEFAULT_VIBRATION_DURATION = 500;
 
+    private DeviceUtils() {
+    }
+
+    private static void vibrate(Context context) {
+        vibrate(context, DEFAULT_VIBRATION_DURATION);
+    }
+
+    private static void vibrate(Context context, long millisec) {
+        if (context == null) {
+            throw new IllegalArgumentException("Argument 'millisec' cannot be null.");
+        }
+        millisec = millisec > DEFAULT_VIBRATION_DURATION ? millisec : DEFAULT_VIBRATION_DURATION;
+        Vibrator v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        v.vibrate(millisec);
     }
 }
